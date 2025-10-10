9e5eee81b3403ee59dd6deee5dc4034d4ef01ebf
tje0714 <tje0714@outlook.kr>
2025-10-10 12:18:52 +0900

    goods select

---
+++ GoodsMall/src/main/java/edu/the/joeun/model/Goods.java
@@ -0,0 +1,21 @@
+package edu.the.joeun.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+// @Entity JPA가 아니기 때문에 사용하지 않음
+public class Goods {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+   // @Column(length=100) JPA가 아니기 때문에 사용하지 않음
+    private String name;
+    private int price;
+    private int stock;
+}
