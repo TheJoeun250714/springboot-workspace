eff4d458d144b2f88285ed47aa10f0e0886e1f51
tje0714 <tje0714@outlook.kr>
2025-10-13 13:18:54 +0900

    user 비밀번호 설정 BCrypto 추가

--- GoodsMall/src/main/java/edu/the/joeun/service/UserService.java
+++ GoodsMall/src/main/java/edu/the/joeun/service/UserService.java
@@ -4,6 +4,7 @@ package edu.the.joeun.service;
 import edu.the.joeun.mapper.UsersMapper;
 import edu.the.joeun.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,20 +15,29 @@ public class UserService {
     @Autowired
     private UsersMapper usersMapper;
 
+    // 다른 개발자가 만든 비밀번호 암호화 생성할 수 있는 객체
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
     public List<User> getAllUser(){
         return usersMapper.getAllUser();
     }
 
     public void insertUser(User user){
-        /**
-         * 비밀번호 암호화 같은 복합 작업 진행하는 공간
-         * 프로필 사진을 폴더에 저장하고 폴더 경로도  DB에 저장 가능
-         */
-        // user.setRole("USER");
-        // html 에서 클라이언트가 작성설정을 하는 것이 아니라
-        // 개발자의 회사에서 클라이언트 정보를 강제적으로
-        // 기본값 설정해줄 때 추가 로직을 작성할 수 있음
+
+        String 유저작성패스워드 = user.getPassword();
+        String 암호화처리패스워드 = bCryptPasswordEncoder.encode(유저작성패스워드);
+
+        user.setPassword(암호화처리패스워드); // 암호화 처리된 패스워드로 교체하여 user DB 저장하도록 설정
+
         usersMapper.insertUser(user);
     }
+    /**
+     * 비밀번호 암호화 같은 복합 작업 진행하는 공간
+     * 프로필 사진을 폴더에 저장하고 폴더 경로도  DB에 저장 가능
+     */
+    // user.setRole("USER");
+    // html 에서 클라이언트가 작성설정을 하는 것이 아니라
+    // 개발자의 회사에서 클라이언트 정보를 강제적으로
+    // 기본값 설정해줄 때 추가 로직을 작성할 수 있음
 
 }
