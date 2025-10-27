e2c549d63a4b42fabdbc0b9255541b3e4c736ac8
tje0714 <tje0714@outlook.kr>
2025-10-27 13:20:00 +0900

    board 게시물 전체 조회

--- GoodsCommunity/src/main/resources/static/js/board.js
+++ GoodsCommunity/src/main/resources/static/js/board.js
@@ -29,9 +29,13 @@ async  function fetchBoardData(){
                 const row = document.createElement("tr");
 
                 row.innerHTML=`
-<td>${b.id}</td>
-                <td class="title-cell" onclick="openModal(${b.id})">${b.title}</td>
+                <td>${b.id}</td>
+                <td class="title-cell">${b.title}</td>
                 <td>${b.writer}</td>
+                <td></td>
+                <td></td>
+                <td onclick="openModal(${b.id})">미리보기</td>
+                <td onclick="gotoDetail(${b.id})">상세보기</td>
                 
                 `;
                 tbody.appendChild(row);
@@ -43,7 +47,40 @@ async  function fetchBoardData(){
 
 }
 
-// 게시글 상세보기 (모달열기)
+// 게시글 상세보기 를 호출하는 기능
+async  function detailFunction(id){
+    const res = await  fetch(API_BASE_URL + `/board/${id}`);
+
+    if(!res.ok) {
+        throw new Error("게시글을 불러오는데 실패했습니다.");
+    }
+  return  await res.json(); // detailFunction 을 이용하면 백엔드에서 호출한 데이터가 반환
+}
+
+// 상세보기 페이지에서 실행할 기능
+async  function fetchBoardDetail(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const boardId = urlParams.get("id");
+
+    // 클라이언트가 DB에 존재하지 않는 id를 작성할 때 게시물 메인으로 돌려보내기
+    if(!boardId) { 
+        alert("잘못된 게시글 번호입니다.");
+        window.location.href = "/board";
+    }
+
+    const b = await  detailFunction(boardId);
+
+    const title = document.querySelector(".board-title");
+    title.textContent = b.title;
+}
+
+
+
+// 모달열기 기능
+/**
+ * 모달에서 게시글 상세보기
+ * @param id  id - 게시글 아이디
+ */
 async function openModal(id) {
     const modal = document.getElementById("viewModal");
     const modalTitle = document.getElementById("modalTitle");
@@ -53,9 +90,9 @@ async function openModal(id) {
 
     modal.style.display ="flex";
 
-    const res = await  fetch(API_BASE_URL + `/board/${id}`);
+   // const res = await  fetch(API_BASE_URL + `/board/${id}`);
 
-    const board = await  res.json();
+    const board = await  detailFunction(id);
 
     modalTitle.textContent = board.title;
     modalInfo.textContent = `작성자 ${board.writer}`;
@@ -67,5 +104,30 @@ function  closeModal(){
     modal.style.display = "none";
 }
 
+/**
+ *
+ * 페이지를 구분해서 함수 실행
+ */
 // 페이지 로드 시 자동으로 특정 기능 실행
-window.addEventListener("DOMContentLoaded", fetchBoardData);
+window.addEventListener("DOMContentLoaded", () => {
+    if(document.getElementById("boardTable")){
+        fetchBoardData();
+    }
+    if (document.querySelector(".board-detail-container")) {
+        fetchBoardDetail();
+    }
+});
+
+
+// 상세페이지 이동 버튼 기능
+function  gotoDetail(id) {
+    window.location.href = `/board/detail?id=${id}`;
+}
+// 목록으로 돌아가기 버튼 기능
+function  gotoList() {
+    window.location.href = `/board`;
+}
+
+// 모달열기로 해놓은 상세보기를
+// detailFunction 으로 기능 분리 후
+// 이 기능을 모달열기와 상세보기페이지에서 사용할 수 있도록 변경
