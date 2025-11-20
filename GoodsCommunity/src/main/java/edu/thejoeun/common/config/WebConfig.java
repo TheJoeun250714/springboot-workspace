0767c64ab99dbec4a1efa1bfa28e602a348fcc86
tje0714 <tje0714@outlook.kr>
2025-11-20 12:34:36 +0900

    websocket

--- GoodsCommunity/src/main/java/edu/thejoeun/common/config/WebConfig.java
+++ GoodsCommunity/src/main/java/edu/thejoeun/common/config/WebConfig.java
@@ -19,7 +19,15 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // REST API CORS 설정
                 registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:57007","http://localhost:3000")
+                        .allowCredentials(true)
+                        .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
+                        .allowedHeaders("*");
+
+                // WebSocket CORS 설정 추가
+                registry.addMapping("/ws/**")
                         .allowedOrigins("http://localhost:57007","http://localhost:3000")
                         .allowCredentials(true)
                         .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
