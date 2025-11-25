1579240c18e0be3fbb082b194c260f97a809b16b
tje0714 <tje0714@outlook.kr>
2025-11-25 13:20:16 +0900

    product

--- GoodsCommunity/src/main/java/edu/thejoeun/product/model/dto/Product.java
+++ GoodsCommunity/src/main/java/edu/thejoeun/product/model/dto/Product.java
@@ -17,7 +17,20 @@ public class Product {
     private String description ;
     private String manufacturer;
     private String imageUrl;
+    /*
     private boolean isActive ;
+    mysql 의 경우 boolean 타입 존재
+    oracle 의 경우 boolean 타입 없으므로
+    char 를 이용하여 'Y' 'N' 형태를 주로 사용
+
+    만약 isActive 를 boolean 타입으로 제공한다면
+    serviceImpl 에서
+    isActive 가 'Y' 일 경우 boolean true 로 변환하여
+    frontend 에 전달할 수 있으나,
+    private String isActive 로 타입을 전달받아
+    활용하는 것이 용이
+     */
+    private String isActive ;
     private String createdAt ;
     private String  updatedAt;
 }
