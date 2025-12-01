dc5887faba7358cf39636021b0acedd4f8c2d545
tje0714 <tje0714@outlook.kr>
2025-12-01 10:21:06 +0900

    프로필 이미지 업로드

--- GoodsCommunity/src/main/java/edu/thejoeun/common/config/SecurityConfig.java
+++ GoodsCommunity/src/main/java/edu/thejoeun/common/config/SecurityConfig.java
@@ -26,17 +26,38 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-/*
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
 
+                /**
+                 * 권한 설정 - 모든 요청 허용
+                 * 현재는 개발 단계이기 때문에 특정 api나 url 경로 설정에 대하여
+                 * 접근 제한을 하지 않고 모두 아이디 비밀번호 없이 접근 가능하도록 설정하나,
+                 * 모든 프로젝트를 마무리하고 배포작업을 진행하기 전에는
+                 * 커스텀 URL 접근 설정을 진행해야한다.
+                 */
+                .authorizeHttpRequests( auth -> auth
+                        .anyRequest().permitAll())
+
+                /**
+                 * 특정 페이지는 아이디 비밀번호 없이 접근 가능하도록 권한 설정
+                 * 정적 리소스는 인증 없이 접근 가능
+                 * 리액트로 진행하는 프론트엔드 또한 빌드작업을 하여
+                 * resource 와 static 으로 정적데이터를 가져오면
+                 * 하위 폴더에 접근할 수 있는 권한이 존재해야하기 때문에 미리 작성
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/profile_images/**").permitAll()
+                        .requestMatchers("/profile_images/**",
+                                        "/css/**",
+                                        "/js/**",
+                                        "/images/**"
+                                ).permitAll()
                         .anyRequest().authenticated()
 
                 )
+                */
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout
                         .logoutUrl("/api/auth/logout")
@@ -45,7 +66,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-
- */
 
 }
