95e457c76f5bd48c06c876085da3a96e85ef7a63
tje0714 <tje0714@outlook.kr>
2025-12-01 16:51:56 +0900

    프로필이미지

--- GoodsCommunity/src/main/java/edu/thejoeun/member/controller/MemberAPIController.java
+++ GoodsCommunity/src/main/java/edu/thejoeun/member/controller/MemberAPIController.java
@@ -55,13 +55,24 @@ public class MemberAPIController {
     // mapper.xml -> mapper.java -> service.java -> serviceImpl.java apiController.java
     // 완성
 
+    /**
+     * @RequestPart  : multipart/form-data 파일 + JSON 파트 데이터 받을 때
+     * @RequestParam : URL 쿼리 파라미터 / HTML Form 파라미터
+     * @RequestBody  : 요청 전체를 객체로 받을 때
+     *
+     * required = false 는 @PathVariable @RequestPart @RequestParam @RequestBody
+     * 모두에서 쓸 수 있는 속성으로 각 데이터가 필수로 존재하지 않아도 될 때 사용
+     * 기본값은 true
+     * @param member
+     */
     @PostMapping("/signup")
-    public void saveSignup(@RequestBody Member member){
+    public void saveSignup(@RequestPart Member member,
+                           @RequestPart(required=false) MultipartFile profileImage){
       log.info("===회원가입 요청===");
       log.info("요청 데이터 - 이름 : {}, 이메일 : {}",member.getMemberName(),member.getMemberEmail());
 
       try {
-          memberService.saveMember(member);
+          memberService.saveMember(member, profileImage);
           log.info("회원가입 성공 - 이메일 : {}",member.getMemberEmail());
       } catch (Exception e){
           log.error("회원가입 실패 - 이메일 : {}, 에러 : {}",member.getMemberEmail(),e.getMessage());
@@ -80,7 +91,7 @@ public class MemberAPIController {
      * @param session
      * @return
      */
-    @PostMapping("/update")
+    @PutMapping("/update")
     public Map<String, Object> updateMypage(@RequestBody Map<String, Object> updateData, HttpSession session){
         log.info("회원정보 수정 요청");
         try{
